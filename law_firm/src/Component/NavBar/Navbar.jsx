4d78d97b49ca0bb02fe6af3be4825732313ea054
ShivamSh002 <shivamsharma98879@gmail.com>
2024-02-02 19:12:58 +0530

    commit

--- law_firm/src/Component/NavBar/Navbar.jsx
+++ law_firm/src/Component/NavBar/Navbar.jsx
@@ -4,16 +4,16 @@ import { ContactButton } from '../ContactButton/ContactButton'
 import styles from "./NavBar.module.css"
 import {HashLink as Link} from "react-router-hash-link"
 
+const linkStyle = { color: 'white', textDecoration: 'none' };
 const Navbar = () => {
   return (
    
-    
     <div className={styles.wrapper} > <div><Logo/></div>
     <div className={styles.header}>
-    <Link to="#home" smooth> <h4 style={{ color: 'white', textDecoration: 'none' }} >Home</h4></Link>
-    <Link to="#team" smooth> <h4 style={{ color: 'white', textDecoration: 'none' }} >Attorneys</h4></Link>
-    <Link to="#pratice" smooth> <h4 style={{ color: 'white', textDecoration: 'none' }} >Practice Areas</h4></Link>
-    <Link to="#about" smooth> <h4 style={{ color: 'white', textDecoration: 'none' }} >About Us</h4></Link>
+    <Link to="#home" smooth style={linkStyle}><h4>Home</h4></Link>
+        <Link to="#team" smooth style={linkStyle}><h4>Attorneys</h4></Link>
+        <Link to="#practice" smooth style={linkStyle}><h4>Practice Areas</h4></Link>
+        <Link to="#about" smooth style={linkStyle}><h4>About Us</h4></Link>
     
     </div>
     <Link to="#contact" smooth><div><ContactButton/></div></Link>
