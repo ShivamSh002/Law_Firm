f033451074b2379d0607a32f3ff8fc13fa9ac1bc
ShivamSh002 <shivamsharma98879@gmail.com>
2024-02-02 19:14:44 +0530

    commit

--- law_firm/src/Component/Footer/Footer.jsx
+++ law_firm/src/Component/Footer/Footer.jsx
@@ -7,13 +7,16 @@ import XIcon from '@mui/icons-material/X';
 import PinterestIcon from '@mui/icons-material/Pinterest';
 import {HashLink as Link} from "react-router-hash-link"
 
+const linkStyle = { color: 'white', textDecoration: 'none' };
+
 const Footer = () => {
   return (
     <div id="contact"><div className={styles.wrapper} > <div><Logo/></div>
-    <div className={styles.header}> <Link to="#home" smooth> <h4 style={{ color: 'white', textDecoration: 'none' }} >Home</h4></Link>
-    <Link to="#team" smooth> <h4 style={{ color: 'white', textDecoration: 'none' }} >Attorneys</h4></Link>
-    <Link to="#pratice" smooth> <h4 style={{ color: 'white', textDecoration: 'none' }} >Practice Areas</h4></Link>
-    <Link to="#about" smooth> <h4 style={{ color: 'white', textDecoration: 'none' }} >About Us</h4></Link>
+    <div className={styles.header}> 
+    <Link to="#home" smooth style={linkStyle}><h4>Home</h4></Link>
+        <Link to="#team" smooth style={linkStyle}><h4>Attorneys</h4></Link>
+        <Link to="#practice" smooth style={linkStyle}><h4>Practice Areas</h4></Link>
+        <Link to="#about" smooth style={linkStyle}><h4>About Us</h4></Link>
     </div>
     <div className={styles.icons}>
         <InstagramIcon sx={{ fontSize: 30, color: '#E3B748' }}/>
